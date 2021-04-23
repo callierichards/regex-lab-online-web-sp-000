320a2474bb9c1513946466f1b2aa7b108f7a65e7
callie rogers <callie1013@gmail.com>
2021-04-23 01:56:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,10 @@ else
 end
 
 def valid_phone_number?(phone)
-phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  return true
+else
+  false
 end
 
 end
